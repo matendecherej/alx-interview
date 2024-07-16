638305899b57aff22f12f4c88a252664a3182123
matendecherej <matendecherej@gmail.com>
2024-07-16 23:26:10 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,42 +1,28 @@
 #!/usr/bin/python3
-"""Maria and Ben are playing a game"""
+"""Prime game module.
+"""
 
 
 def isWinner(x, nums):
-    """x - number of rounds
-    nums - numbers of list
+    """Determines the winner of a prime game session with `x` rounds.
     """
-    if x <= 0 or nums is None:
+    if x < 1 or not nums:
         return None
-    if x != len(nums):
+    marias_wins, bens_wins = 0, 0
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
         return None
-
-    ben_var = 0
-    maria_var = 0
-
-    b = [1 for x in range(sorted(nums)[-1] + 1)]
-    b[0], b[1] = 0, 0
-    for i in range(2, len(b)):
-        remove_multiples(b, i)
-
-    for i in nums:
-        if sum(b[0:i + 1]) % 2 == 0:
-            ben_var += 1
-        else:
-            maria_var += 1
-    if ben_var > maria_var:
-        return "Ben"
-    if maria_var > ben_var:
-        return "Maria"
-    return None
-
-
-
-def remove_multiples(ls, x):
-    """removes multiple of primes
-    """
-    for j in range(2, len(ls)):
-        try:
-            ls[j * x] = 0
-        except (ValueError, IndexError):
-            break
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
